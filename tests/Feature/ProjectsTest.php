4626369dcbc42c66156915196da5b8610a1c46ea
unknown <ahedqaood19@gmail.com>
2020-05-15 00:34:40 +0300

    assemble all guest related Tests into guests_cannot_manage_projects Test

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -11,25 +11,14 @@ class ProjectsTest extends TestCase
     use WithFaker, RefreshDatabase;
     
     /** @test */
-    public function guests_cannot_view_projects_creat_projects()
+    public function guests_cannot_manage_projects()
     {
-        $attributes = factory('App\Project')->raw();
-    
-        $this->post('/projects', $attributes)->assertRedirect('login');
-    }
-
-    /** @test */
-    public function guests_cannot_view_projects()
-    {    
-        $this->get('/projects')->assertRedirect('login');
-    }
-
-    /** @test */
-    public function guests_cannot_view_a_single_project()
-    {    
         $project = factory('App\Project')->create();
-
+        
+        $this->get('/projects')->assertRedirect('login');
+        $this->get('/projects/create')->assertRedirect('login');
         $this->get($project->path())->assertRedirect('login');
+        $this->post('/projects', $project->toArray())->assertRedirect('login');
     }
 
     /** @test */
