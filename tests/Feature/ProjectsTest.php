23257154de22100376dcc53ff9eb5885d9c3f9cc
unknown <ahedqaood19@gmail.com>
2020-05-14 22:33:29 +0300

    add guests_cannot_view_a_single_project Test + minor changes on other tests

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -25,6 +25,14 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
+    public function guests_cannot_view_a_single_project()
+    {    
+        $project = factory('App\Project')->create();
+
+        $this->get($project->path())->assertRedirect('login');
+    }
+
+    /** @test */
     public function a_user_can_create_a_project()
     {
         $this->withoutExceptionHandling();
@@ -44,11 +52,13 @@ class ProjectsTest extends TestCase
     }
 
     /** @test */
-    public function a_user_can_view_a_project()
+    public function a_user_can_view_their_project()
     {
+        $this->be(factory('App\User')->create());
+
         $this->withoutExceptionHandling();
 
-        $project = factory('App\Project')->create();
+        $project = factory('App\Project')->create(['owner_id' => auth()->id()]);
 
         $this->get('/projects/'. $project->id)
             ->assertSee($project->title)
