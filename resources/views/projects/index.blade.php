b9be83318d161e7eab87c79610a2c16c8973c3b6
unknown <ahedqaood19@gmail.com>
2020-05-19 02:32:25 +0300

    some edits on the design

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -5,14 +5,15 @@
         <a href="/projects/create">New Project</a>
     </div>
 
-    <ul>
+    <div class="flex">
         @forelse ($projects as $project)
-            <li>
-                <a href="{{ $project->path() }}">{{ $project->title }}</a>
-            </li>
-        
+            <div class="bg-white mr-4 p-5 rounded shadow w-1/3" style="height: 200px">
+                <h3 class="font-normal text-xl py-4">{{ str_limit($project->title, 20) }}</h3>
+                
+                <div class="text-gray-500">{{ str_limit($project->description, 70) }}</div>
+            </div>
         @empty
-            <li>No projects yet!</li>
+            <div>No projects yet!</div>
         @endforelse
-    </ul>
+    </div>
 @endsection
