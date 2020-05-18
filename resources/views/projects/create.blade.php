5fece7e1251145300bfbee445b02d81c81a394d1
unknown <ahedqaood19@gmail.com>
2020-05-19 00:15:05 +0300

    redesign layouts

--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -1,16 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.2/css/bulma.min.css">
-</head>
-<body>
-    
-
-    <form method="POST" action="/projects" class="container" style="padding-top: 40px">
+@extends('layouts.app')
+@section('content')
+    <form method="POST" action="/projects">
         @csrf
         
         <h1 class="heading is-l">Create a Project</h1>
@@ -37,5 +27,4 @@
             </div>
         </div>
     </form>
-</body>
-</html>
+@endsection
