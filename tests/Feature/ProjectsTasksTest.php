ce8c23833d6e63ce786b3aca920d5060693cd8a7
unknown <ahedqaood19@gmail.com>
2020-05-23 01:34:23 +0300

    add only_the_owner_of_a_project_can_add_tasks Test

--- tests/Feature/ProjectsTasksTest.php
+++ tests/Feature/ProjectsTasksTest.php
@@ -12,6 +12,27 @@ class ProjectsTasksTest extends TestCase
     use RefreshDatabase, WithFaker;
 
     /** @test */
+    public function guests_cannot_add_tasks_to_projects()
+    {
+        $project = factory('App\Project')->create();
+
+        $this->post($project->path() . '/tasks')->assertRedirect('login');
+    }
+
+    /** @test */
+    public function only_the_owner_of_a_project_can_add_tasks()
+    {
+        $this->signIn();
+
+        $project = factory('App\Project')->create();
+
+        $this->post($project->path() . '/tasks', ['body' => 'Test Task'])
+            ->assertStatus(403);
+
+        $this->assertDataBaseMissing('tasks', ['body' => 'Test Task']);
+    }
+
+    /** @test */
     public function a_project_can_have_tasks()
     {
         $this->withoutExceptionHandling();
